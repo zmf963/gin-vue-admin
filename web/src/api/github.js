cb19f805215817dc47ba48b725741c06ebac725c
Gitea <gitea@fake.local>
2022-01-08 00:38:41 +0800

    ✨ feat(web): 移除多余的前端页面

--- web/src/api/github.js
+++ web/src/api/github.js
@@ -4,14 +4,14 @@ const service = axios.create()
 
 export function Commits(page) {
   return service({
-    url: 'https://api.github.com/repos/flipped-aurora/gin-vue-admin/commits?page=' + page,
+    url: 'https://api.github.com/repos/zmf963/gin-vue-admin/commits?page=' + page,
     method: 'get'
   })
 }
 
 export function Members() {
   return service({
-    url: 'https://api.github.com/orgs/FLIPPED-AURORA/members',
+    url: 'https://api.github.com/orgs/zmf963/members',
     method: 'get'
   })
 }
